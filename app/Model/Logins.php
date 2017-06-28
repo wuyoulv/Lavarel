0d3ef9023b9fcbd9a1b75805e68ac7396c70db5e
617140924 <617140924@qq.com>
2017-06-28 15:55:35 +0800

    sxy测试

---
+++ app/Model/Logins.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Logins extends Model
+{
+    protected $table="login";
+    protected $login="login";
+}
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get("admin/user_film","Admin\User_filmController@index");//用户视频中间表
+Route::get("admin/logins","Admin\LoginsController@login");//登录信息
 Route::get("/login","Admin\LoginController@index");
 Route::get("admin/getcode","Admin\LoginController@getCode");//加载验证码
 Route::post("admin/dologin","Admin\LoginController@dologin");
