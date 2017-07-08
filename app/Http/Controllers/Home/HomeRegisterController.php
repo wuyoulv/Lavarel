07edd29a72fe6fa81a52a42ce5fe8158c3c0d491
wuhanwang <761312801@qq.com>
2017-07-08 15:22:04 +0800

    前台登陆

--- app/Http/Controllers/Home/HomeRegisterController.php
+++ app/Http/Controllers/Home/HomeRegisterController.php
@@ -162,7 +162,7 @@ class HomeRegisterController extends Controller
     }
     
     public function sendSms(Request $request){
-        $phone = $request ->input('phone','18119986561'); // 用户手机号，接收验证码
+        $phone = $request ->input('account'); // 用户手机号，接收验证码
         $name = '兄弟连';  // 短信签名,可以在阿里大鱼的管理中心看到
         $num = rand(100000, 999999); // 生成随机验证码
         $smsParams = [
@@ -173,9 +173,9 @@ class HomeRegisterController extends Controller
         //$request ->session()->put('alidayu',$num);  // 存入session 后面做数据验证
 
         $result=$this->sms->send($phone,$name,$content,$code);
-        echo "<pre>";
-        var_dump($result);die;
-        echo "验证码：".session('alidayu').'<br/>';
+        //echo "<pre>";
+        //var_dump($result);die;
+        //echo "验证码：".session('alidayu').'<br/>';
         if(property_exists($request,'result')){
            // 使用PHP函数json_encode方法将给定数组转化为JSON：
             return json_encode(['ResultData' => '成功', 'info' => '已发送']);
